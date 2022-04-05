d70b56baf632d29cdd98900a3b3f31fb20fd4a55
Jhoisan Allendes <jhoisan.allendes@usach.cl>
2022-04-04 21:52:26 -0400

    Creacion TDA card

---
+++ TDA_card.rkt
@@ -0,0 +1,15 @@
+#lang racket
+(provide (all-defined-out))
+;; TDA cardsSet ;;
+;Representacion:
+; (list element1 element2 ... elementN)
+
+;; Capa Constructora ;;
+;Descripcion: Funcion Constructora de una carta
+;Dom: list X int X randomFn
+;Rec: card
+(define card
+  (lambda (list int rndFn)
+    (cond
+      [(null? lista) null] 
+      [()])))
