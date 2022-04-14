00bf5a7caf2bcad8f1aeb0165742b87d975e8c59
Jhoisan Allendes <jhoisan.allendes@usach.cl>
2022-04-14 19:08:17 -0400

    implementacion de requiredElements, nthCard, numCards y findTotalCards

--- TDA_cardsSet.rkt
+++ TDA_cardsSet.rkt
@@ -4,23 +4,23 @@
 (require "TDA_card.rkt")
 ;;; TDA cardsSet ;;;
 ;;Representacion;;
-; (list card1 card2 ... cardN)
+; (list card1 X card2 X ... X cardN X lista de Elementos)
 
 
 ;; Capa Constructora ;;
-;Descripcion: Constructor de set de cartas
+;Descripcion: Constructor de set de cartas.
 ;Dom: list(lista de elementos) X int(numero de elementos en cada carta) X int(Maximo de cartas) X randomFn(Funcion de aleatorizacion)
 ;Rec: cardsSet(TDA)
 (define cardsSet
   (λ (listaElementos cantElemento maxCartas rndFn)
-    (rndFn (createCardsSet listaElementos cantElemento maxCartas)
+    (append (rndFn (createCardsSet listaElementos cantElemento maxCartas)
               '()
               1
               (length (createCardsSet listaElementos cantElemento maxCartas))
               (valRandom (createCardsSet listaElementos cantElemento maxCartas)
                          (random (length (createCardsSet listaElementos cantElemento maxCartas)))
                          )
-              )
+              ) (list listaElementos))
     )
   )
 
@@ -33,20 +33,20 @@
 
 
 ;; Funcion de utilidad ;;
-;Descripcion: crear lista de cartas
+;Descripcion: crear lista de cartas.
 ;Dom: lista de elementos(list) X elementos por carta(int) X maximo de cartas(int) X contador(int) X contador(int) X contador(int)
 ;Rec: lista de cartas(list)
 (define createCardsSet
   (λ (listaElementos elementosXCarta MCards)
     (cond
-      [(< MCards 0) (append (list (firstCard listaElementos elementosXCarta 1 '())) (nCards listaElementos elementosXCarta 1 1 '() '()) (n2Cards listaElementos elementosXCarta 1 1 1 '() '()))]
+      [(<= MCards 0) (append (list (firstCard listaElementos elementosXCarta 1 '())) (nCards listaElementos elementosXCarta 1 1 '() '()) (n2Cards listaElementos elementosXCarta 1 1 1 '() '()))]
       [(> MCards 0) (acortarLista (createCardsSet listaElementos elementosXCarta -1) '() 1 MCards)]
       )
     )
   )
 
 
-;Descripcion: crear n cartas
+;Descripcion: crear n cartas.
 ;Dom: lista de elementos(list) X elementos por carta(int) X contador(int) X contador(int) X lista de cartas(list) X carta(TDA card)
 ;Rec: lista de cartas(list)
 (define nCards
@@ -59,7 +59,7 @@
   )
 
 
-;Descripcion: Crear el resto de cartas
+;Descripcion: Crear el resto de cartas.
 ;Dom: lista de elementos(list) X elementos por carta(int) X contador(int) X contador(int) X contador(int) X lista de cartas(list) X carta(TDA card)
 ;Rec: lista de cartas(list)
 (define n2Cards
@@ -78,7 +78,7 @@
   )
 
 
-;Descripcion: Funcion agregar un elemento a una carta
+;Descripcion: Funcion agregar un elemento a una carta.
 ;Dom: lista de cartas(list) X carta(TDA)
 ;Rec: lista de cartas(list)
 (define addCard
@@ -88,7 +88,7 @@
   )
 
 
-; Descripcion: valida si el numero esta en el rango valido de 0 y el largo de la lista
+; Descripcion: valida si el numero esta en el rango valido de 0 y el largo de la lista.
 ; Dom: lista de cartas(list) X numero random(int)
 ; Rec: numero random valido(int)
 (define valRandom
@@ -102,7 +102,7 @@
   )
 
 
-; Descripcion: Randomiza set de cartas
+; Descripcion: Randomiza set de cartas.
 ; Dom: lista de cartas(list) X lista auxiliar(list) X contador(int) X largo de lista de cartas(int) X numero random entre 0 y el largo de la lista(int)
 ; Rec: lista de cartas(list)
 (define randomFn
@@ -115,7 +115,7 @@
   )
 
 
-; Descripcion: Acorta la lista a un largo determinado
+; Descripcion: Acorta la lista a un largo determinado.
 ; Dom: lista de cartas(list) X lista auxiliar(list) X contador(int) X largo deseado(int)
 ; Rec: lista de cartas(list)
 (define acortarLista
@@ -125,22 +125,80 @@
       [else (cond
               [(eq? i (+ 1 (length lista1))) (reverse lista2)]
               [else (acortarLista lista1 (append (list (selElementoLista lista1 i)) lista2) (+ i 1) x)]
-              )]
-      )
+              )])
+    )
+  )
+
+; Descripcion:  Permite determinar la cantidad de cartas en el set.
+; Dom: cardsSet(TDA)
+; Rec: cantidad de cartas(int)
+(define numCards
+  (λ (cardsSet)
+    (length cardsSet)
     )
   )
 
 
-(define numCards
-  (λ (cardsSet)))
+; Descripcion:  Obtiene la n-ésima (nth) carta desde el conjunto de cartas partiendo desde 0 hasta (totalCartas-1).
+; Dom: cardsSet(TDA) X Posicion de la carta(int)
+; Rec: n-esima carta(TDA)
+(define nthCard
+  (λ (lista n) 
+   (cond 
+    [(eq? 0 n)(car lista)]
+    [else (nthCard (cdr lista) (- n 1))])
+    )
+  )
 
 
-(define l1 (cardsSet (list  1   2   3   4   5   6   7   8   9   10  11  12  13) 4 12 randomFn))
-(define l2 (acortarLista l1 '() 1 5))
+; Descripcion:  Determina la cantidad total de cartas que se deben producir para construir un conjunto válido.
+; Dom: card(TDA)
+; Rec: cantidad total de cartas que se deben producir para construir un conjunto válido(int)
+(define findTotalCards
+  (λ (card)
+    (+ (* (- (length card) 1) (- (length card) 1)) (- (length card) 1) 1)
+    )
+  )
+
+; Descripcion:  Determina la cantidad total de cartas que se deben producir para construir un conjunto válido.
+; Dom: card(TDA)
+; Rec: total de elementos necesarios para poder construir un conjunto válido(int)
+(define requiredElements
+  (λ (card)
+    (+ (* (- (length card) 1) (- (length card) 1)) (- (length card) 1) 1)
+    )
+  )
+
+; Descripcion:  Determina la cantidad total de cartas que se deben producir para construir un conjunto válido.
+; Dom: card(TDA)
+; Rec: cantidad de cartas(TDA)
+(define missingCards
+  (λ (setCards)
+    (map delCard (nthCard setCards 1) (getListaElementos setCards))
+    )
+  )
 
 
+; Descripcion:  Determina la lista de elementos del set de cartas.
+; Dom: cardsSet(TDA)
+; Rec: lista de elementos(TDA)
+(define getListaElementos
+  (λ (setCards)
+    (car (reverse setCards))
+    )
+  )
 
 
+; Descripcion:  eliminar carta de un set de cartas.
+; Dom: cardsSet(TDA)
+; Rec: cardsSet(TDA)
+(define delCard
+  (λ (card listaElementos)
+    (remove card (cardsSet listaElementos (length card) -1 randomFn))
+    )
+  )
 
-
+(define l1 (cardsSet (list  1   2   3   4   5   6   7   8   9   10  11  12  13) 4 0 randomFn))
+(define l2 (cardsSet (list "a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m") 4 4 randomFn))
+(missingCards  l2)
 
--- TDA_game.rkt
+++ TDA_game.rkt
@@ -1,7 +1,6 @@
 #lang racket
 (require "TDA_element.rkt")
 (require "TDA_card.rkt")
-(require "TDA_cardsSet.rkt") 
-(define game
-  (lambda (numPlayers cardsSet Mode rndFn)
-    (list numPlayers cardsSet Mode rndFn)))
+(require "TDA_cardsSet.rkt")
+
+
